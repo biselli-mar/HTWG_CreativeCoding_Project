28d73ec152085b703cdf2936ee18f092eed1dad7
biselli-mar <ma741bis@htwg-konstanz.de>
2023-02-04 13:37:36 +0100

    removed FlowCircular Font

--- textMorph/vars.js
+++ textMorph/vars.js
@@ -1,9 +1,9 @@
 // ======== Font Vars ========
 let fontPool = ["Amatic", "Anton", "Crimson", 
-                "FlowCircular", "LatoBlack", "LatoThin", 
-                "Lobster", "NotoSerifBold", "NotoSerifItalic",
-                "Playfair", "PlayfairBold", "RalewayBold",
-                "RalewayEL", "RubikMono", "SpecialElite", "YesevaOne"];
+                "LatoBlack", "LatoThin", "Lobster", 
+                "NotoSerifBold", "NotoSerifItalic", "Playfair",
+                "PlayfairBold", "RalewayBold", "RalewayEL",
+                "RubikMono", "SpecialElite", "YesevaOne"];
 let loadedFonts = {};
 let firstLetterFontSampleFactor = 0.5;
 let fontSize;

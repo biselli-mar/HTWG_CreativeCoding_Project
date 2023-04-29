243c6d7c1208ce4597213e164ad935d984578597
Marcel <ma741bis@htwg-konstanz.de>
2023-04-29 15:05:50 +0200

    Update jsconfig.js
    probably dont need that

--- jsconfig.js
+++ jsconfig.js
@@ -1,7 +1,6 @@
 {
     "include": [
       "*.js",
-      "libraries/*.js",
-      "c:\\Users\\marce\\.vscode\\extensions\\samplavigne.p5-vscode-1.2.12\\p5types\\global.d.ts"
+      "libraries/*.js"
     ]
   }
